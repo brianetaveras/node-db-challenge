fad9ab7a2d1b19d492685ad2b1dd2ca7e7f9701d
brian <taveras574@gmail.com>
2020-03-08 23:12:34 -0400

    Added middleware to catch erros

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ server.use('/projects', projectRoute)
 server.use('/resources', resourcesRoute)
 server.use('/projects/:id', tasksRoute)
 
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
 
 
 server.listen(4000, ()=>{
